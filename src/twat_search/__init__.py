fa0cbcdda67dadd647a4dd2ca132572611415467
twardoch <adam@twardoch.com>
2025-02-15 06:32:29 +0100

    v1.7.13

--- src/twat_search/__init__.py
+++ src/twat_search/__init__.py
@@ -2,4 +2,6 @@
 
 from importlib import metadata
 
-__version__ = metadata.version(__name__)
+from twat_search.__version__ import version as __version__
+
+__all__ = ["__version__"]
--- tests/test_twat_search.py
+++ tests/test_twat_search.py
@@ -1,6 +1,8 @@
 """Test suite for twat_search."""
 
+
 def test_version():
     """Verify package exposes version."""
     import twat_search
+
     assert twat_search.__version__
